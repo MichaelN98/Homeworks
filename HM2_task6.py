78ebf0a711e3d06d5971aad126fbb86e6b5063a8
MichaelN98 <michnok98@gmail.com>
2023-01-24 12:33:16 +0200

    HM2_task6

---
+++ HM2_task6.py
@@ -0,0 +1,9 @@
+# 6
+group = ["Jack Pie", "Erik Man", "Lili Evans", "Patrick Low", "Lili Evans", "Patrick Low"]
+uniq_group = []
+for item in group:
+    if item not in uniq_group:
+        uniq_group.append(item)
+print(uniq_group)
+
+
