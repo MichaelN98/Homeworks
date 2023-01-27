c6d68ce3958b7e55af343d784d5ea265153550dd
MichaelN98 <michnok98@gmail.com>
2023-01-27 18:30:11 +0200

    HM3_task2

---
+++ HM3_task2.py
@@ -0,0 +1,24 @@
+number_1 = 2
+number_2 = 2
+
+print(number_1 * number_2)
+print(number_1 ** number_2)
+print(number_1 / number_2)
+print(number_1 // number_2)
+
+x = 2
+y = 2
+z = x ^ y
+# print('x = ', x)
+# print('y = ', y)
+print('z = ', z)
+
+
+x = 2
+y = 2
+z = x | y
+# print('x = ', x)
+# print('y = ', y)
+print('z = ', z)
+
+
