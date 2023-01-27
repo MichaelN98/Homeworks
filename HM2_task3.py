760d508fd06e81bba0b29f4b39a4b9c54bf59e57
MichaelN98 <michnok98@gmail.com>
2023-01-27 17:43:55 +0200

    fixed HM2_task3

--- HM2_task3.py
+++ HM2_task3.py
@@ -4,12 +4,19 @@ casino_blacklist = ["Jack Pie", "Erik Man", "Lili Evans"]
 poker_blacklist = ["Patrick Low", "Ann Downstown", "Mari Darret"]
 alcohol_blacklist = ["Albert Second", "Kate Signi", "Iri Mane"]
 
-casino_blacklist.extend(alcohol_blacklist)
-casino_blacklist.extend(alcohol_blacklist)
-print(casino_blacklist)
+for x in [casino_blacklist, alcohol_blacklist]:
+    poker_blacklist.extend(x)
+
+print(poker_blacklist)
 
 # second option
-# all_blacklists = list
 # all_blacklists=(*casino_blacklist, *poker_blacklist, *alcohol_blacklist)
 # print(all_blacklists)
 
+# third option
+# all_blacklists = casino_blacklist + poker_blacklist + alcohol_blacklist
+# print(all_blacklists)
+
+#  fourth option
+# all_blacklists = [j for i in [poker_blacklist, casino_blacklist, alcohol_blacklist] for j in i]
+# print(all_blacklists)
