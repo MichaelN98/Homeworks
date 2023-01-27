a284054e68c8b24ba98dd5d9290fdb4da71fa1c2
MichaelN98 <michnok98@gmail.com>
2023-01-27 18:31:53 +0200

    HM3_task3

---
+++ HM3_task3.py
@@ -0,0 +1,10 @@
+friends = ["John", "Marta", "James"]
+enemies = ["John", "Johnatan", "Artur"]
+
+for friend in friends:
+    if friend != 'James':
+        if friend not in enemies and friend != 'James':
+            print(f'{friend} we are the best friends')
+        else:
+            print(f'{friend} we are not friends anymore')
+
