2a03882a453913fede6f9ceae0951fd1363d7c4b
MichaelN98 <michnok98@gmail.com>
2023-01-30 17:10:51 +0200

    fixed HM3_task1, added work with any data

--- HM3_task1.py
+++ HM3_task1.py
@@ -1,7 +1,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 for index, item in enumerate(numbers):
-    if index == 1 or index == 3 or index == 5 or index == 7:
-        print(index,  'even')
-    if index == 0 or index == 2 or index ==4 or index == 6:
-        print(index, 'odd')
+    if item % 2 == 0:
+        print(index,  'odd')
+    if not item % 2 == 0:
+        print(index, 'even')
 
