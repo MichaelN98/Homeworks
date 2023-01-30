52e4a2ec3467758203d7422038b7e6ce6e065b23
MichaelN98 <michnok98@gmail.com>
2023-01-30 09:18:31 +0200

    fixed HM3_task1 to work with any list of values

--- HM3_task1.py
+++ HM3_task1.py
@@ -1,7 +1,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 for index, item in enumerate(numbers):
-    if item == 1 or item == 3 or item == 5 or item == 7:
-        print(index,  'odd')
-    if item == 0 or item == 2 or item ==4  or item == 6:
-        print(index, 'even')
+    if index == 1 or index == 3 or index == 5 or index == 7:
+        print(index,  'even')
+    if index == 0 or index == 2 or index ==4 or index == 6:
+        print(index, 'odd')
 
