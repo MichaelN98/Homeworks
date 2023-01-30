48b16cf9cdeefe6e7fd8912fe1e8257cbbe1e9a1
MichaelN98 <michnok98@gmail.com>
2023-01-30 17:12:27 +0200

    fixed HM3_task3, removed second check James

--- HM3_task3.py
+++ HM3_task3.py
@@ -3,7 +3,7 @@ enemies = ["John", "Johnatan", "Artur"]
 
 for friend in friends:
     if friend != 'James':
-        if friend not in enemies and friend != 'James':
+        if friend not in enemies:
             print(f'{friend} we are the best friends')
         else:
             print(f'{friend} we are not friends anymore')
