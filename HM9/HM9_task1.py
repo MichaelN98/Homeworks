1bb002f6cf151f2f06f8eedb1904dcd804920fd7
MichaelN98 <michnok98@gmail.com>
2023-03-10 11:45:21 +0200

    fixed tasks HM9

--- HM9/HM9_task1.py
+++ HM9/HM9_task1.py
@@ -1,62 +1,32 @@
-class Any_company:
-    """A class that describes a company"""
+class AnyCompany:
 
-    def __init__(self, name, age, budget):
-        """
-        Initializes a company instance with the given name, age, budget.
-
-        Parameters
-        ----------
-        name : str
-            The name of the company
-        age : int
-            The age of the company
-         budget : int
-            The  budget of  the company
-        """
+    def __init__(self, name: str, age: int, budget: int, type_of_work: str):
         self.__name = name
         self.__age = age
-        self.__budget =  budget
+        self.__budget = budget
+        self.__type_of_work = type_of_work
 
     @property
     def name(self):
-        """
-        Get the name of the company
-        """
+
         return self.__name
 
     @name.setter
     def name(self, name):
-        """
-        Set the name of the company
 
-         Parameters
-         ----------
-         name : str
-         The name of the company with that conditions
-        """
-        if len(name) >= 3:
+        if len(str(name)) >= 3:
             self.__name = name
         else:
             raise ValueError("Name should have at least 3 characters.")
 
     @property
     def age(self):
-        """
-        Get the age of the company
-        """
+
         return self.__age
 
     @age.setter
     def age(self, value):
-        """
-        Set the agw of the company
 
-         Parameters
-         ----------
-         age : int
-         The age of the company with that conditions
-        """
         if not isinstance(value, int):
             raise ValueError("age of the company must be an integer")
         if value < 0:
@@ -66,37 +36,34 @@ class Any_company:
         self.__age = value
 
     @property
-    def  budget(self):
-        """
-        Get the  budget of the company
-        """
+    def budget(self):
+
         return self.__budget
 
     @ budget.setter
-    def  startup(self,  budget):
-        """
-        Set the  budget of the company
+    def budget(self, budget):
 
-         Parameters
-         ----------
-          budget : int
-         The  budget of the company with that conditions
-        """
-        if not isinstance( budget, int):
+        if not isinstance(budget, int):
             raise ValueError(" budget must be an integer")
-        if  budget <= 0:
+        elif budget <= 0:
             raise ValueError(" budget cannot be negative")
-        if  budget < 10000:
-            print(f'{ budget} is a startup')
-        if  budget >= 10000:
-            print(f'{ budget} is a business')
+        elif budget < 10000:
+            print(f'{budget} is a startup')
+        elif budget >= 10000:
+            print(f'{budget} is a business')
+        self.__budget = budget
 
     @staticmethod
     def bonus_of_timework(years_of_work):
-        """
-        Add bonus for work in the company
-        Parameters
-         ----------
-         bonus : int
-        """
+
         return years_of_work * 0.5
+
+    @classmethod
+    def create_company_name_budget(cls, name, budget):
+        return cls(name, budget)
+
+    def show_name(self):
+        print(self.__name, 'is name of the company')
+
+    def type_of_work(self):
+        return self.__type_of_work
--- HM9/HM9_task2.py
+++ HM9/HM9_task2.py
@@ -1,62 +1,32 @@
 class Worker:
-    """A class that describes a worker"""
 
-    def __init__(self, name, age, salary):
-        """
-        Initializes a company instance with the given name, age and salary.
+    def __init__(self, name: str, age: int, salary: int, type_work: str):
 
-        Parameters
-        ----------
-        name : str
-            The name of the company
-        age : int
-            The age of  the worker is in
-        salary : int
-            The salary of worker at the company
-        """
         self.__name = name
         self.__age = age
         self.__salary = salary
-
+        self.type_work = type_work
     @property
     def name(self):
-        """
-        Get the name of the company
-        """
+
         return self.__name
 
     @name.setter
-    def name(self, name):
-        """
-        Set the name of the company
+    def name(self, name: str):
 
-         Parameters
-         ----------
-         name : str
-         The name of the company with that conditions
-        """
-        if len(name) >= 2:
+        if len(str(name)) >= 2:
             self.__name = name
         else:
             raise ValueError("Name should have at least 2 characters.")
 
     @property
     def age(self):
-        """
-        Get the age of the worker
-        """
+
         return self.__age
 
     @age.setter
-    def age(self, age):
-        """
-        Set the agw of the company
+    def age(self, age: int):
 
-         Parameters
-         ----------
-         age : int
-         The age of the worker with that conditions
-        """
         if not isinstance(age, int):
             raise ValueError("age of the worker must be an integer")
         elif age < 0:
@@ -75,35 +45,25 @@ class Worker:
 
     @property
     def salary(self):
-        """
-        Get the salary of the worker
-        """
+
         return self.__salary
 
     @salary.setter
-    def salary(self, salary):
-        """
-        Set the salary of the worker
+    def salary(self, salary: int):
 
-         Parameters
-         ----------
-        salary : int
-         The the salary of the workers with that conditions
-        """
         if salary <= 1000:
             print(f'{salary} is small salary')
-        if salary >= 1000 and salary <= 2000:
+        elif salary >= 1000 and salary <= 2000:
             print(f'{salary} is good salary')
-        if salary > 2000 :
+        elif salary > 2000:
             print(f'{salary} is big salary')
+        self.__salary = salary
 
     @staticmethod
-    def time_work(years_of_work):
-        """
-        Add bonus for work in the company if worked more 8 years
-        Parameters
-         ----------
-         years_of_work : int
-        """
+    def time_work(years_of_work: int):
+
         if years_of_work > 8:
             return years_of_work * 0.5
+
+    def what_level(self) :
+        return self.age * self.salary
