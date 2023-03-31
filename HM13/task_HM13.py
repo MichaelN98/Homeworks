e06b26e9cb5e98d336d3b19e52ca8c4378cc3f88
MichaelN98 <michnok98@gmail.com>
2023-03-31 09:10:32 +0300

    change fix of task from HM13

--- HM13/task_HM13.py
+++ HM13/task_HM13.py
@@ -3,15 +3,14 @@ class CustomIterator:
         self.__sequence = sequence
         self.__start_index = start_index
         self.__end_index = end_index
-        self.__current_position = start_index
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.__start_index <= self.__current_position <= self.__end_index:
-            value = self.__sequence[self.__current_position]
-            self.__current_position += 1
+        if self.__start_index < self.__end_index and self.__start_index < len(self.__sequence) and len(str(self.__start_index)) <= 1:
+            value = self.__sequence[self.__start_index]
+            self.__start_index += 1
             return value
         else:
             raise StopIteration
